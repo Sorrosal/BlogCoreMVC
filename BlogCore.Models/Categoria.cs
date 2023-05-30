6e34a5c0a0ab58e53fa43a3a3d6ea2d0597db1ed
Sorrosal <sergiosorrosalgayan@gmail.com>
2023-05-30 20:12:10 +0200

    Agregar archivos de proyecto.

---
+++ BlogCore.Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogCore.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Ingrese un nombre para la categoría")]
+        [Display(Name = "Nombre Categoría")]
+        public string Nombre { get; set; }
+        [Display(Name = "Orden de visualización")]
+        public int? Orden { get; set; }
+    }
+}
---
+++ BlogCore/Areas/Cliente/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BlogCore.Areas.Cliente.Controllers
+{
+    [Area("Cliente")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
